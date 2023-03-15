6937b52ac2d466f46b5a8e6946128528171f8e39
Alex Cani <alex.consulting.ag@hexagon.com>
2023-03-15 19:08:19 -0300

    Basic async/await usage. block_on executor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use futures::executor::block_on;
+
+async fn hello_world() {
+    println!("Async hello world!")
+}
+
+async fn a() {
+    println!("A");
+}
+
+async fn b() {
+    println!("B");
+}
+
+async fn c() {
+    a().await;
+    b().await;
+}
+
+async fn d() {
+    futures::join!(c(), hello_world());
+}
+
 fn main() {
-    println!("Hello, world!");
+    block_on(d())
 }
